ab6248204ebf3f5135c38998a659291714f345fe
dileepchil1995 <dilip.chilakapati1995@gmail.com>
2020-09-09 12:56:12 +1000

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBfTcmS3_xjB1WWF8VvFg-yj37QhPmA7kM",
+    authDomain: "clone-13708.firebaseapp.com",
+    databaseURL: "https://clone-13708.firebaseio.com",
+    projectId: "clone-13708",
+    storageBucket: "clone-13708.appspot.com",
+    messagingSenderId: "520232335946",
+    appId: "1:520232335946:web:4aeb9018330254511f84b3",
+    measurementId: "G-L686W4V7GR"
+  };
