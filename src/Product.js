546afbe9f8356ab7349bc0880937b040764a103c
dileepchil1995 <dilip.chilakapati1995@gmail.com>
2020-10-03 10:55:28 +1000

    Updates

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import './Product.css';
+import { useStateValue } from './StateProvider';
 
 function Product({id, title, image, price, rating}) {
+    const [{ basket }, dispatch] = useStateValue();
+
+    const addToBasket = () => {
+        // data layer
+    }
+
+
+
     return (
         <div className="product">
             <div className="product_info">
