25fad0a6c090cb8ca1f62be1e240c2216b48fe4d
dileepchil1995 <dilip.chilakapati1995@gmail.com>
2020-10-03 10:57:20 +1000

    Updates

--- src/Checkout.js
+++ src/Checkout.js
@@ -9,7 +9,7 @@ function Checkout() {
                 <img 
                 className="checkout_ad"
                 src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
-                alt=""
+                alt="CHECKOUT"
                 />
                 <div>
                     <h2 className="checkout_title">Your Shopping Basket</h2>
